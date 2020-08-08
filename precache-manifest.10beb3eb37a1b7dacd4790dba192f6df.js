c18ca48bb5290949a943ad53edb02cc7d5a6e49d
afni <afnisakinah@gmail.com>
2020-08-08 11:10:59 +0700

    Updates

---
+++ precache-manifest.10beb3eb37a1b7dacd4790dba192f6df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6731a9b83d4d87592543ac5750e3a25b",
+    "url": "/Klasifikasi/index.html"
+  },
+  {
+    "revision": "d33c413dbd87560b6b16",
+    "url": "/Klasifikasi/static/css/main.2d4a5306.chunk.css"
+  },
+  {
+    "revision": "abde61ee11ffbb5f4789",
+    "url": "/Klasifikasi/static/js/2.24208b1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Klasifikasi/static/js/2.24208b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33c413dbd87560b6b16",
+    "url": "/Klasifikasi/static/js/main.3eb7be55.chunk.js"
+  },
+  {
+    "revision": "a21696f470c0e292a307",
+    "url": "/Klasifikasi/static/js/runtime-main.fd28fba2.js"
+  }
+]);
